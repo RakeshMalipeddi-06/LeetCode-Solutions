35038993d4f27442cbfa6f0df88bd579791391c7
Rakesh <rakeshmalipeddi06@gmail.com>
2026-09-14 20:26:14 +0530

    Time: 28 ms (52.03%), Space: 26.1 MB (23.93%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -8,8 +8,8 @@ class Solution(object):
         for i in nums:
             if i in s:
                 return True
-            else:
-                s.add(i)
+            
+            s.add(i)
         
         return False
         
